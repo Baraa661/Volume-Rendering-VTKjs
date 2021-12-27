9c00c1820dcbd6e09682ab3ed6a2443a05a1ab30
Baraa661 <61358818+Baraa661@users.noreply.github.com>
2021-12-27 22:23:25 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,206 +1,79 @@
 import '@kitware/vtk.js/favicon';
 
 // Load the rendering pieces we want to use (for both WebGL and WebGPU)
-import '@kitware/vtk.js/Rendering/Profiles/Volume';
+import '@kitware/vtk.js/Rendering/Profiles/Geometry';
 
-import vtkColorTransferFunction from '@kitware/vtk.js/Rendering/Core/ColorTransferFunction';
+// Force DataAccessHelper to have access to various data source
+import '@kitware/vtk.js/IO/Core/DataAccessHelper/HtmlDataAccessHelper';
+import '@kitware/vtk.js/IO/Core/DataAccessHelper/HttpDataAccessHelper';
+import '@kitware/vtk.js/IO/Core/DataAccessHelper/JSZipDataAccessHelper';
+
+import vtkActor from '@kitware/vtk.js/Rendering/Core/Actor';
 import vtkFullScreenRenderWindow from '@kitware/vtk.js/Rendering/Misc/FullScreenRenderWindow';
 import vtkHttpDataSetReader from '@kitware/vtk.js/IO/Core/HttpDataSetReader';
-import vtkPiecewiseFunction from '@kitware/vtk.js/Common/DataModel/PiecewiseFunction';
-import vtkPiecewiseGaussianWidget from '@kitware/vtk.js/Interaction/Widgets/PiecewiseGaussianWidget';
-import vtkVolume from '@kitware/vtk.js/Rendering/Core/Volume';
-import vtkVolumeMapper from '@kitware/vtk.js/Rendering/Core/VolumeMapper';
+import vtkImageMarchingCubes from '@kitware/vtk.js/Filters/General/ImageMarchingCubes';
+import vtkMapper from '@kitware/vtk.js/Rendering/Core/Mapper';
 
-// Force the loading of HttpDataAccessHelper to support gzip decompression
-import '@kitware/vtk.js/IO/Core/DataAccessHelper/HttpDataAccessHelper';
+const controlPanel = `
+<table>
+  <tr>
+    <td>Iso value</td>
+    <td>
+      <input class='isoValue' type="range" min="0.0" max="1.0" step="0.05" value="0.0" />
+    </td>
+  </tr>
+</table>
+`;
 
-import vtkColorMaps from '@kitware/vtk.js/Rendering/Core/ColorTransferFunction/ColorMaps';
-
-// ----------------------------------------------------------------------------
-// Standard rendering code setup
-// ----------------------------------------------------------------------------
-
-const rootContainer = document.querySelector(
-  '.vtk-js-example-piecewise-gaussian-widget'
-);
-const containerStyle = rootContainer ? { height: '100%' } : null;
-const urlToLoad = rootContainer
-  ? rootContainer.dataset.url ||
-    'https://kitware.github.io/vtk-js/data/volume/LIDC2.vti':
-    `https://kitware.github.io/vtk-js/data/volume/LIDC2.vti`;
-
-const fullScreenRenderer = vtkFullScreenRenderWindow.newInstance({
+const fullScreenRenderWindow = vtkFullScreenRenderWindow.newInstance({
   background: [0, 0, 0],
-  rootContainer,
-  containerStyle,
 });
-const renderer = fullScreenRenderer.getRenderer();
-const renderWindow = fullScreenRenderer.getRenderWindow();
+const renderWindow = fullScreenRenderWindow.getRenderWindow();
+const renderer = fullScreenRenderWindow.getRenderer();
 
-renderWindow.getInteractor().setDesiredUpdateRate(15.0);
+fullScreenRenderWindow.addController(controlPanel);
 
-// ----------------------------------------------------------------------------
-// Example code
-// ----------------------------------------------------------------------------
-
-const body = rootContainer || document.querySelector('body');
-
-// Create Widget container
-const widgetContainer = document.createElement('div');
-widgetContainer.style.position = 'absolute';
-widgetContainer.style.top = 'calc(10px + 1em)';
-widgetContainer.style.left = '5px';
-widgetContainer.style.background = 'rgba(255, 255, 255, 0.3)';
-body.appendChild(widgetContainer);
-
-// Create Label for preset
-const labelContainer = document.createElement('div');
-labelContainer.style.position = 'absolute';
-labelContainer.style.top = '5px';
-labelContainer.style.left = '5px';
-labelContainer.style.width = '100%';
-labelContainer.style.color = 'white';
-labelContainer.style.textAlign = 'center';
-labelContainer.style.userSelect = 'none';
-labelContainer.style.cursor = 'pointer';
-body.appendChild(labelContainer);
-
-let presetIndex = 1;
-const globalDataRange = [0, 255];
-const lookupTable = vtkColorTransferFunction.newInstance();
-
-function changePreset(delta = 1) {
-  presetIndex =
-    (presetIndex + delta + vtkColorMaps.rgbPresetNames.length) %
-    vtkColorMaps.rgbPresetNames.length;
-  lookupTable.applyColorMap(
-    vtkColorMaps.getPresetByName(vtkColorMaps.rgbPresetNames[presetIndex])
-  );
-  lookupTable.setMappingRange(...globalDataRange);
-  lookupTable.updateRange();
-  labelContainer.innerHTML = vtkColorMaps.rgbPresetNames[presetIndex];
-}
-
-let intervalID = null;
-function stopInterval() {
-  if (intervalID !== null) {
-    clearInterval(intervalID);
-    intervalID = null;
-  }
-}
-
-labelContainer.addEventListener('click', (event) => {
-  if (event.pageX < 200) {
-    stopInterval();
-    changePreset(-1);
-  } else {
-    stopInterval();
-    changePreset(1);
-  }
-});
-
-// ----------------------------------------------------------------------------
-// Example code
-// ----------------------------------------------------------------------------
-
-const widget = vtkPiecewiseGaussianWidget.newInstance({
-  numberOfBins: 256,
-  size: [400, 150],
-});
-widget.updateStyle({
-  backgroundColor: 'rgba(255, 255, 255, 0.6)',
-  histogramColor: 'rgba(100, 100, 100, 0.5)',
-  strokeColor: 'rgb(0, 0, 0)',
-  activeColor: 'rgb(255, 255, 255)',
-  handleColor: 'rgb(50, 150, 50)',
-  buttonDisableFillColor: 'rgba(255, 255, 255, 0.5)',
-  buttonDisableStrokeColor: 'rgba(0, 0, 0, 0.5)',
-  buttonStrokeColor: 'rgba(0, 0, 0, 1)',
-  buttonFillColor: 'rgba(255, 255, 255, 1)',
-  strokeWidth: 2,
-  activeStrokeWidth: 3,
-  buttonStrokeWidth: 1.5,
-  handleWidth: 3,
-  iconSize: 20, // Can be 0 if you want to remove buttons (dblClick for (+) / rightClick for (-))
-  padding: 10,
-});
-
-fullScreenRenderer.setResizeCallback(({ width, height }) => {
-  widget.setSize(Math.min(450, width - 10), 150);
-});
-
-const piecewiseFunction = vtkPiecewiseFunction.newInstance();
-
-const actor = vtkVolume.newInstance();
-const mapper = vtkVolumeMapper.newInstance({ sampleDistance: 1.1 });
-const reader = vtkHttpDataSetReader.newInstance({ fetchGzip: true });
-
-reader.setUrl(urlToLoad).then(() => {
-  reader.loadData().then(() => {
-    const imageData = reader.getOutputData();
-    const dataArray = imageData.getPointData().getScalars();
-    const dataRange = dataArray.getRange();
-    globalDataRange[0] = dataRange[0];
-    globalDataRange[1] = dataRange[1];
-
-    // Update Lookup table
-    changePreset();
-
-    // Automatic switch to next preset every 5s
-    if (!rootContainer) {
-      intervalID = setInterval(changePreset, 5000);
-    }
-
-    widget.setDataArray(dataArray.getData());
-    widget.applyOpacity(piecewiseFunction);
-
-    widget.setColorTransferFunction(lookupTable);
-    lookupTable.onModified(() => {
-      widget.render();
-      renderWindow.render();
-    });
-
-    renderer.addVolume(actor);
-    renderer.resetCamera();
-    renderer.getActiveCamera().elevation(70);
-    renderWindow.render();
-  });
+const actor = vtkActor.newInstance();
+const mapper = vtkMapper.newInstance();
+const marchingCube = vtkImageMarchingCubes.newInstance({
+  contourValue: 0.0,
+  computeNormals: true,
+  mergePoints: true,
 });
 
 actor.setMapper(mapper);
-mapper.setInputConnection(reader.getOutputPort());
+mapper.setInputConnection(marchingCube.getOutputPort());
 
-actor.getProperty().setRGBTransferFunction(0, lookupTable);
-actor.getProperty().setScalarOpacity(0, piecewiseFunction);
-actor.getProperty().setInterpolationTypeToFastLinear();
+function updateIsoValue(e) {
+  const isoValue = Number(e.target.value);
+  marchingCube.setContourValue(isoValue);
+  renderWindow.render();
+}
 
-// ----------------------------------------------------------------------------
-// Default setting Piecewise function widget
-// ----------------------------------------------------------------------------
+const reader = vtkHttpDataSetReader.newInstance({ fetchGzip: true });
+marchingCube.setInputConnection(reader.getOutputPort());
 
-widget.addGaussian(0.425, 0.5, 0.2, 0.3, 0.2);
-widget.addGaussian(0.75, 1, 0.3, 0, 0);
+reader
+  .setUrl('https://kitware.github.io/vtk-js/data/volume/headsq.vti', { loadData: true })
+  .then(() => {
+    const data = reader.getOutputData();
+    const dataRange = data.getPointData().getScalars().getRange();
+    const firstIsoValue = (dataRange[0] + dataRange[1]) / 3;
 
-widget.setContainer(widgetContainer);
-widget.bindMouseListeners();
+    const el = document.querySelector('.isoValue');
+    el.setAttribute('min', dataRange[0]);
+    el.setAttribute('max', dataRange[1]);
+    el.setAttribute('value', firstIsoValue);
+    el.addEventListener('input', updateIsoValue);
 
-widget.onAnimation((start) => {
-  if (start) {
-    renderWindow.getInteractor().requestAnimation(widget);
-  } else {
-    renderWindow.getInteractor().cancelAnimation(widget);
-  }
-});
-
-widget.onOpacityChange(() => {
-  widget.applyOpacity(piecewiseFunction);
-  if (!renderWindow.getInteractor().isAnimating()) {
+    marchingCube.setContourValue(firstIsoValue);
+    renderer.addActor(actor);
+    renderer.getActiveCamera().set({ position: [1, 1, 0], viewUp: [0, 0, -1] });
+    renderer.resetCamera();
     renderWindow.render();
-  }
-});
+  });
 
-// ----------------------------------------------------------------------------
-// Expose variable to global namespace
-// ----------------------------------------------------------------------------
-
-global.widget = widget;
+global.fullScreen = fullScreenRenderWindow;
+global.actor = actor;
+global.mapper = mapper;
+global.marchingCube = marchingCube;
